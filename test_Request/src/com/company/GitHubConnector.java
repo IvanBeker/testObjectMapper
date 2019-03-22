743f1299b46335de1a1bd876b0ec06d5ab242164
LAPTOP-01VRFI8J\Іван Бекер <ivan.beker@magnise.com>
2019-03-22 13:42:23 +0200

    no message

--- test_Request/src/com/company/GitHubConnector.java
+++ test_Request/src/com/company/GitHubConnector.java
@@ -1,30 +1,48 @@
 package com.company;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class GitHubConnector {
+
+    //region Constants
+
     public final static String GITHUB_API = "https://api.github.com/";
     public final static String GITHUB_USER_API = "https://api.github.com/users/";
-    static String getUserStringJSON(String userName) throws IOException {
-        String userJSON = "";
 
-        URL myUrl = new URL(GITHUB_USER_API+userName);
-        System.out.print(userName);
-        URLConnection yc = myUrl.openConnection();
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(yc.getInputStream()));
-        String inputLine = "";
-        while ((inputLine = in.readLine()) != null) userJSON += inputLine;
+    //endregion Constants
 
-        in.close();
+    //endregion Public
 
-        return userJSON;
+    public static GitHubUserModel getUserModelByName(String userName) throws IOException {
+        StringBuilder userJson = new StringBuilder();
+
+        URL gitHubUserUrl = new URL(GITHUB_USER_API + userName);
+
+        URLConnection yc = gitHubUserUrl.openConnection();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
+        String inputLine;
+        while ((inputLine = bufferedReader.readLine()) != null)
+            userJson.append(inputLine);
+        bufferedReader.close();
+
+        return parseUserModel(userJson.toString());
     }
 
+    //endregion Public
 
+    //region Private
+
+    public static GitHubUserModel parseUserModel(String json) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, GitHubUserModel.class);
+    }
+
+    //endregion Private
 }
--- test_Request/src/com/company/Main.java
+++ test_Request/src/com/company/Main.java
@@ -1,46 +1,29 @@
 package com.company;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-
-import java.net.*;
-import java.io.*;
-import java.lang.*;
-
-
-import java.net.URL;
-import java.util.Map;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("start");
-        System.out.println();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Name: ");
-        String userName = scanner.next();
-        String json = null;
+        String userName = getUserName();
+
+        GitHubUserModel userModel = null;
         try {
-             json = GitHubConnector.getUserStringJSON(userName);
+            userModel = GitHubConnector.getUserModelByName(userName);
         }catch (IOException e){
             System.out.print(e.toString());
             return;
         }
-        GitHubUser gitHub = null;
-        try {
-             gitHub = GitHubUser.getMap(json);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        System.out.print(gitHub.toString());
 
+        System.out.print(userModel.toString());
     }
 
+    private static String getUserName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
 
-
+        return scanner.next();
+    }
 
 }
